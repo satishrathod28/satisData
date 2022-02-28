3af64569c62b58e3770d244af6eab09668e77d27
satishrathod28 <100399342+satishrathod28@users.noreply.github.com>
2022-02-28 22:44:25 +0530

    One section is done

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,24 +1,52 @@
+import { FormControlUnstyledContext } from '@mui/base';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import React, { useState } from 'react';
 import '../css/index.css'; 
 const Todo = () =>{
      const [initial, setInitial] = useState('');
      const [updateData, setUpdateData] = useState([]);
+     const [isId, setIsId]  = useState(null);
+     const [isToggle, setIsToggle] = useState(true)
      const addData = () =>{
       if(initial == ""){
          alert("Please fill the Data");
       }
+      
       else{
-        setUpdateData([...updateData, initial])
-        setInitial([]);
+      const totalData = {id : new Date().getMilliseconds().toString(), name:initial}
+      setUpdateData([...updateData,totalData ])
+
+      setInitial('');
       }
         
      }
+     const modifieData = () =>{
+        setUpdateData(
+            updateData.map((curr)=>{
+                 if(curr.id === isId){
+                     return {...curr, name:initial}
+                 }
+                 return curr
+            })
+        );
+        setInitial('');
+        setIsToggle(true);
+     }
      const deleteData = (id)=>{
-          const newData = updateData.filter((curr, index)=>{
-                return id !== index
+          const newData = updateData.filter((curr)=>{
+                return id !== curr.id
           })
     setUpdateData(newData);
+     }
+     const editData =(id) =>{
+        const newEditData = updateData.filter((curr)=>{
+              if(id === curr.id) {
+                  setInitial(curr.name)
+              }
+             
+      } )
+     setIsToggle(false)
+      setIsId(id)
      }
 return(
 <>
@@ -28,16 +56,21 @@ return(
 <div className='inputData'>
 <input type="text" name="list" id="list" className='mt15' value ={initial}
  onChange={(event)=> setInitial(event.target.value)}/>
-<i className="fa fa-plus" onClick = {addData}></i>
+ {
+     isToggle ? <i className="fa fa-plus" onClick = {addData} title={`Add ${initial} `}></i>:
+     <i className="fa fa-edit" onClick = {modifieData} title={`update ${initial} `}></i>
+ }
+
 </div>
 
 <div className='datalist'>
 {
-    updateData.map((currElement, index)=>{
+    updateData.map((currElement)=>{
         return (
             <>
-             <div className='well well-sm mt15 mb15 text-left mt15 mb15 relative' key={index}>{currElement}<i className="fa fa-file">
-                 </i> <i className="fa fa-trash-o" onClick={() => deleteData(index)}></i> </div>
+             <div className='well well-sm mt15 mb15 text-left mt15 mb15 relative' key={currElement.id}>{currElement.name}
+             <i className="fa fa-file" onClick={() => editData(currElement.id)} title="Edit List"></i> 
+                 <i className="fa fa-trash-o" onClick={() => deleteData(currElement.id)} title={`Remove ${currElement.name}`}></i> </div>
             </>
         )
     })
